9ee48dd23c1f7005bbd1299e9f267dc644ca0f69
Alexina Chong <alexina.chong@gmail.com>
2018-06-06 11:46:32 -0700

    Add initial actions and reducer

--- src/actions/profile_actions.js
+++ src/actions/profile_actions.js
@@ -3,6 +3,8 @@ export const ADD_BIRTHDAY = 'ADD_BIRTHDAY';
 export const ADD_GENDER = 'ADD_GENDER';
 export const ADD_RELATIONSHIP = 'ADD_RELATIONSHIP';
 
+export const GO_TO_NEXT_PAGE = 'GO_TO_NEXT_PAGE';
+
 export const addName = name => ({
   type: ADD_NAME,
   name: name
@@ -21,4 +23,10 @@ export const addGender = gender => ({
 export const addRelationship = relationship => ({
   type: ADD_RELATIONSHIP,
   relationship: relationship
+});
+
+// add action to keep track of current page - check this
+export const goToNextPage = page => ({
+  type: GO_TO_NEXT_PAGE,
+  page: page
 });
--- src/reducers/profiles_reducer.js
+++ src/reducers/profiles_reducer.js
@@ -3,7 +3,16 @@ import { ADD_NAME, ADD_BIRTHDAY, ADD_GENDER, ADD_RELATIONSHIP } from '../actions
 
 const profilesReducer = (state = {}, action) => {
   Object.freeze(state);
-  
+
+  switch(action.type) {
+    case ADD_NAME:
+      return;
+
+
+
+    default:
+      return state;
+  }
 };
 
 export default profilesReducer;
