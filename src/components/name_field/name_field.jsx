96b46a7aa5179c2b170db899e9dc566072a90f44
Alexina Chong <alexina.chong@gmail.com>
2018-06-06 21:27:03 -0700

    Set up basic name field structure

---
+++ src/components/name_field/name_field.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class NameField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="name-field-container">
+        <h2>What is the child's <strong>name</strong>?</h2>
+
+        <label>
+          <input
+            type="text"
+            value={this.state.firstName}
+            onChange={this.update('firstName')}
+            placeholder="Steve"
+            className="profile-input"
+            id="first-name-field"
+            autoFocus
+          />
+        </label>
+        
+        <label>
+          <input
+            type="text"
+            value={this.state.lastName}
+            onChange={this.update('lastName')}
+            placeholder="Kiwi"
+            className="profile-input"
+            id="last-name-field"
+          />
+        </label>
+      </div>
+    );
+  }
+
+}
+
+export default NameField;
