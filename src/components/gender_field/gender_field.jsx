63b23a81eefe2270854f9817c559a1820353c55c
Alexina Chong <alexina.chong@gmail.com>
2018-06-09 23:50:49 -0700

    Add props to display firstName and persist selection to state

--- src/components/gender_field/gender_field.jsx
+++ src/components/gender_field/gender_field.jsx
@@ -4,15 +4,25 @@ class GenderField extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // what is default gender?
-      // gender: ''
+      gender: ''
     };
+  }
+
+  update(field) {
+    return e =>
+      this.setState({
+        [field]: e.currentTarget.value
+      });
+  }
+
+  componentWillUnmount() {
+    this.props.addGender(this.state.gender);
   }
 
   render() {
     return (
       <div className="gender-field-container">
-        <h2>Select this.props.profiles.names <strong>gender</strong>:</h2>
+        <h2>Select {this.props.profiles.firstName}&#x27;s <strong>gender</strong>:</h2>
 
         <div className="gender-field-options-container">
           <img src="http://icdn.kiwicrate.com/site/account/gender-boy.png" className="gender-field-options" />
--- src/components/gender_field/gender_field_container.js
+++ src/components/gender_field/gender_field_container.js
@@ -1,16 +1,16 @@
 import { connect } from 'react-redux';
-// import actions
+import { addGender } from '../../actions/profile_actions';
 import GenderField from './gender_field';
 
 const mapStateToProps = (state, ownProps) => {
   return {
-
+    profiles: state.profiles
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-
+    addGender: gender => dispatch(addGender(gender))
   };
 };
 
