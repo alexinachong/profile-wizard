13e3bd91a85a89f37e0ed71e375c075b388a662d
Alexina Chong <alexina.chong@gmail.com>
2018-06-10 15:30:33 -0700

    Convert static gender images into radio button options

--- src/components/gender_field/gender_field.jsx
+++ src/components/gender_field/gender_field.jsx
@@ -25,8 +25,15 @@ class GenderField extends React.Component {
         <h2>Select {this.props.profiles.firstName}&#x27;s <strong>gender</strong>:</h2>
 
         <div className="gender-field-options-container">
-          <img src="http://icdn.kiwicrate.com/site/account/gender-boy.png" className="gender-field-options" />
-          <img src="http://icdn.kiwicrate.com/site/account/gender-girl.png" className="gender-field-options" />
+          <label>
+            <input type="radio" name="gender" value="male" onChange={this.update('gender')}/>
+            <img src="http://icdn.kiwicrate.com/site/account/gender-boy.png" className="gender-field-options" />
+          </label>
+
+          <label>
+            <input type="radio" name="gender" value="female" onChange={this.update('gender')} />
+            <img src="http://icdn.kiwicrate.com/site/account/gender-girl.png" className="gender-field-options" />
+          </label>
         </div>
       </div>
     );
